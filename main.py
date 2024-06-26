ebff9b3c0e15a8f64c334d133d30551f1f99a677
Ariel Jaimez <arieljaimez@gmail.com>
2024-06-26 13:44:48 -0300

    Se cambio las comillas y la funcion que devuelva el json pedido por CFIA

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import urllib.request
 import os
 from werkzeug.utils import secure_filename
 from werkzeug.exceptions import HTTPException
+from datetime import timezone 
+import datetime 
 
 # import cv2  # OpenCV for image processing
 # import matplotlib.pyplot as plt  # Matplotlib for visualization
@@ -18,120 +20,122 @@ import ssl
 ssl._create_default_https_context = ssl._create_stdlib_context
 
 import logging
-logging.basicConfig(filename='error.log',level=logging.DEBUG)
+logging.basicConfig(filename="error.log",level=logging.DEBUG)
 
 app = Flask(__name__)
  
-UPLOAD_FOLDER = 'static/uploads/'
+UPLOAD_FOLDER = "static/uploads/"
  
 app.secret_key = "secret key"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
  
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
-ALLOWED_EXTENSIONS_PDF = set(['pdf'])
-ALLOWED_EXTENSIONS_XML = set(['xml'])
+ALLOWED_EXTENSIONS = set(["png", "jpg", "jpeg"])
+ALLOWED_EXTENSIONS_PDF = set(["pdf"])
+ALLOWED_EXTENSIONS_XML = set(["xml"])
+
+def returnJSON_OK(claveNumerica,nombreArchivo,extension):
+    _returnData = { 
+                    "data" : {
+                            "claveNumerica" : claveNumerica,
+                            "fechaConsulta" : datetime.datetime.now(timezone.utc),
+                            "nombreArchivo" : "",
+                            "extension" : ""
+                        },
+                     "mensaje" : {
+                         "codigo" : 1,
+                         "descripcion" : ""
+                     }
+                    }
+    return jsonify(_returnData)
+
+def returnJSON_ERROR(erroDescripcion):
+    _returnData = { 
+            "data" : "",
+                     "mensaje" : {
+                         "codigo" : -1,
+                         "descripcion" : erroDescripcion
+                     }
+        }
+    return jsonify(_returnData)
+
 
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def allowed_file_pdf(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS_PDF
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS_PDF
      
 def allowed_file_xml(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS_XML
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS_XML
  
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/pdf')
+@app.route("/pdf")
 def homepdf():
-    return render_template('indexpdf.html')
+    return render_template("indexpdf.html")
 
-@app.route('/xml')
+@app.route("/xml")
 def homexml():
-    return render_template('indexxml.html')
+    return render_template("indexxml.html")
 
-@app.route('/xml', methods=['POST'])
+@app.route("/xml", methods=["POST"])
 def upload_xml():
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash('No XML selected for uploading')
-        return redirect(request.url)
+    if "file" not in request.files:
+        return returnJSON_ERROR("No file part")
+    file = request.files["file"]
+    if file.filename == "":
+        return returnJSON_ERROR("No XML selected for uploading")
     if file and allowed_file_xml(file.filename):
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
 
-        clave = XMLProcess(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        clave = XMLProcess(os.path.join(app.config["UPLOAD_FOLDER"], filename))
 
-        data = { 
-            "Data" : clave, 
-        }
-        return jsonify(data) 
+        return returnJSON_OK(clave,"","") 
     else:
-        data = { 
-            "Data" : "ERROR Allowed XML", 
-        }
-        return jsonify(data)    
+        return returnJSON_ERROR("ERROR Allowed XML")    
 
-@app.route('/pdf', methods=['POST'])
+@app.route("/pdf", methods=["POST"])
 def upload_pdf():
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash('No PDF selected for uploading')
-        return redirect(request.url)
+    if "file" not in request.files:        
+        return  returnJSON_ERROR("No file part")
+    file = request.files["file"]
+    if file.filename == "":
+        return returnJSON_ERROR("No PDF selected for uploading")
     if file and allowed_file_pdf(file.filename):
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
 
-        clave = PDFProcess(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        clave = PDFProcess(os.path.join(app.config["UPLOAD_FOLDER"], filename))
 
-        data = { 
-            "Data" : clave, 
-        }
-        return jsonify(data) 
-    else:
-        data = { 
-            "Data" : "ERROR Allowed PDF", 
-        }
-        return jsonify(data) 
+        return returnJSON_OK(clave , "" , "") 
+    else:        
+        return returnJSON_ERROR("ERROR Allowed PDF") 
 
-@app.route('/image', methods=['POST'])
+@app.route("/image", methods=["POST"])
 def upload_image():
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash('No image selected for uploading')
-        return redirect(request.url)
+    if "file" not in request.files:
+        return  returnJSON_ERROR("No file part")
+    file = request.files["file"]
+    if file.filename == "":        
+        return returnJSON_ERROR("No image selected for uploading")
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
 
-        clave = OCRProces(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        clave = OCRProces(os.path.join(app.config["UPLOAD_FOLDER"], filename))
 
-        data = { 
-            "Data" : clave, 
-        }
-        return jsonify(data)
-    else:
-        data = { 
-            "Data" : "ERROR Allowed image types are - png, jpg, jpeg", 
-        }
-        return jsonify(data)
+        return returnJSON_OK(clave , "" , "")
+    else:        
+        return returnJSON_ERROR("ERROR Allowed image types are - png, jpg, jpeg")
  
-@app.route('/display/<filename>')
+@app.route("/display/<filename>")
 def display_image(filename):
-    #print('display_image filename: ' + filename)
-    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+    #print("display_image filename: " + filename)
+    return redirect(url_for("static", filename="uploads/" + filename), code=301)
 
 
 def XMLProcess(xml_path):
@@ -149,7 +153,7 @@ def XMLProcess(xml_path):
     return "NO DATA"
 
 def PDFProcess(pdf_payh):
-    pdfFileObject = open(pdf_payh,'rb')
+    pdfFileObject = open(pdf_payh,"rb")
 
     pdfReader = PyPDF2.PdfReader(pdfFileObject)
 
@@ -174,7 +178,7 @@ def OCRProces(image_path):
     # image = cv2.imread(image_path)
 
     # Initializing the EasyOCR reader with English language support and GPU disabled
-    reader = easyocr.Reader(['la','es'], gpu=False, model_storage_directory = 'easyocr_model',user_network_directory=False)
+    reader = easyocr.Reader(["la","es"], gpu=False, model_storage_directory = "easyocr_model",user_network_directory=False)
 
     # Extracting text from the image
     results = reader.readtext(image_path)
@@ -183,13 +187,13 @@ def OCRProces(image_path):
 
     # Displaying the extracted results
     for detection in results:
-        # if detection[1].startswith('Clave'): #or detection[1].startswith('506') :
+        # if detection[1].startswith("Clave"): #or detection[1].startswith("506") :
         #     print(detection[1])
         #     break
-        if 'Clave' in detection[1]:
+        if "Clave" in detection[1]:
             arr_str.append(detection[1])
 
-        if detection[1].startswith('506'):            
+        if detection[1].startswith("506"):            
             if(len(detection[1])>45):
                 arr_str.append(detection[1])
                 break
@@ -214,4 +218,4 @@ def getNumeric(data):
     return _data
 
 if __name__ == "__main__":
-    app.run(host= '0.0.0.0', port=5000,debug=True)
+    app.run(host= "0.0.0.0", port=5000,debug=True)
